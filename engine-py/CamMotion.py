6c94e4e334dee9eeac3f2e8d1a1040424d5cbe4a
Nat Morris <nat@nuqe.net>
2012-09-16 22:45:56 +0100

    Moved mocap into its own class

---
+++ engine-py/CamMotion.py
@@ -0,0 +1,77 @@
+import FeedConfig
+from PIL import Image, ImageChops
+import time
+import math
+
+class CamMotion:
+ """Motion capture stuff"""
+ cfg = None
+ cw = None
+ appeared = False
+ appearedframe = -1
+ appearedsecs = -1
+ frames = []
+ 
+ def __init__(self,cfg,cw):
+  self.cfg = cfg
+  self.cw = cw
+  return
+  
+ def capture(self):
+		# load some settings
+		capsecs = self.cfg.getfloat("motion","capturesecs")
+		entropylevel = self.cfg.getfloat("motion","entropy")
+		extendsecs = self.cfg.getfloat("motion","extendsecs")
+		betweenframes = self.cfg.getfloat("motion","betweenframes")
+		saveframes = self.cfg.getboolean("motion","saveframes")
+		# store results here
+		frames = []
+		frameappeared = -1
+		timeappeared = ""
+		taken = 0
+		frame = 0
+		started = time.time()
+		frames.append(self.cw.SnapCam1()) # grab first shot
+
+		while taken < capsecs:
+			time.sleep(betweenframes)
+			frames.append(self.cw.SnapCam1())
+			taken = time.time() - started
+			frame = frame + 1
+			
+			if saveframes == True:
+				frames[len(frames)-1].save(("img%s.jpg" % (frame)),"JPEG")
+			
+			# calculate difference between the images
+			img = ImageChops.difference(frames[len(frames)-1],frames[len(frames)-2])
+			ent = self.image_entropy(img)
+			print "f%s %0.2fe %0.1fs / %0.1fs" % (frame,ent,taken,capsecs)
+			
+			if frameappeared == -1:
+				if float(ent) > entropylevel:
+					frameappeared = frame
+					timeappeared = taken
+					print "appeared."
+					if (capsecs - taken) < extendsecs:
+						capsecs = capsecs + extendsecs
+						print "extending cap seconds"
+						
+		self.frames = frames				
+		if frameappeared == -1:
+		 self.appeared = False
+		else:
+		 self.appeared = True
+		 self.appearedframe = frameappeared
+		 self.appearedsecs = timeappeared
+
+  
+  
+ # Function below is from: http://stackoverflow.com/questions/5524179/how-to-detect-motion-between-two-pil-images-wxpython-webcam-integration-exampl
+ # Credit: http://stackoverflow.com/users/31676/paul
+ def image_entropy(self,img):
+		"""calculate the entropy of an image"""
+		# this could be made more efficient using numpy
+		histogram = img.histogram()
+		histogram_length = sum(histogram)
+		samples_probability = [float(h) / histogram_length for h in histogram]
+		return -sum([p * math.log(p, 2) for p in samples_probability if p != 0])
--- engine-py/img.py
+++ engine-py/img.py
@@ -3,62 +3,13 @@
 import CamWrapper
 import CamMotion
 import FeedConfig
-from PIL import Image, ImageChops
-import time
-import math
-
-# Function below is from: http://stackoverflow.com/questions/5524179/how-to-detect-motion-between-two-pil-images-wxpython-webcam-integration-exampl
-# Credit: http://stackoverflow.com/users/31676/paul
-def image_entropy(img):
-    """calculate the entropy of an image"""
-    # this could be made more efficient using numpy
-    histogram = img.histogram()
-    histogram_length = sum(histogram)
-    samples_probability = [float(h) / histogram_length for h in histogram]
-    return -sum([p * math.log(p, 2) for p in samples_probability if p != 0])
-
 
 fc = FeedConfig.FeedConfig("live.cfg")
 cw = CamWrapper.CamWrapper(fc)
+cm = CamMotion.CamMotion(fc,cw)
 
-# load some settings
-capsecs = fc.getfloat("motion","capturesecs")
-entropylevel = fc.getfloat("motion","entropy")
-extendsecs = fc.getfloat("motion","extendsecs")
-betweenframes = fc.getfloat("motion","betweenframes")
-saveframes = fc.getboolean("motion","saveframes")
-frames = []
-frameappeared = -1
-timeappeared = ""
-taken = 0
-frame = 0
-started = time.time()
-frames.append(cw.SnapCam1())
-loop = True
-while taken < capsecs:
- time.sleep(betweenframes)
- frames.append(cw.SnapCam1())
- taken = time.time() - started
- frame = frame + 1
- 
- if saveframes == True:
-  frames[len(frames)-1].save(("img%s.jpg" % (frame)),"JPEG")
- 
- # calculate difference between the images
- img = ImageChops.difference(frames[len(frames)-1],frames[len(frames)-2])
- ent = image_entropy(img)
- print "f%s %0.2fe %0.1fs / %0.1fs" % (frame,ent,taken,capsecs)
- 
- if frameappeared == -1:
-  if float(ent) > entropylevel:
-   frameappeared = frame
-   timeappeared = taken
-   print "appeared."
-   if (capsecs - taken) < extendsecs:
-    capsecs = capsecs + extendsecs
-    print "extending cap seconds"
-
-if frameappeared == -1:
- print "didn't appeared"
+cm.capture()
+if cm.appeared == False:
+ print "Didn't appear"
 else:
- print "appeared at f%s @ %0.2fs" % (frameappeared,timeappeared)
+ print "appeared at f%s @ %0.2fs" % (cm.appearedframe,cm.appearedsecs)
